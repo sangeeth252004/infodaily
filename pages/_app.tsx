c37bd8a0e02a6b2fc79295ca91e94a2c43c79cfb
sangeeth252004 <kvesangeeth@gmail.com>
2026-02-08 14:45:54 +0530

    Add Head component with external script in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { Analytics } from '@vercel/analytics/react'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <script src="https://pl28673557.effectivegatecpm.com/85/11/89/85118992f0933623f335d164c50928aa.js"></script>
+      </Head>
+
       <Component {...pageProps} />
       <Analytics />
     </>
